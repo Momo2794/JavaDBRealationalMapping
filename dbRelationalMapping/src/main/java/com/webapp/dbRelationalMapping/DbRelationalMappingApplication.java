26e449c123896566e2df8796f63c8837bd1b8cff
Momo2794 <mohamed_haddad@hotmail.de>
2021-02-21 13:43:55 +0100

    Raw Project Upload

---
+++ dbRelationalMapping/src/main/java/com/webapp/dbRelationalMapping/DbRelationalMappingApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.dbRelationalMapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbRelationalMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbRelationalMappingApplication.class, args);
+	}
+
+}
